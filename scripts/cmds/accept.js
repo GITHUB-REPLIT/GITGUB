4bde8fae9bbd19903e7640b5816ceff82b75877a
GITHUB-REPLIT <aryanvts@yahoo.com>
2024-09-14 16:40:53 +0600

    Update accept.js

--- scripts/cmds/accept.js
+++ scripts/cmds/accept.js
@@ -1,3 +1,4 @@
+const { GoatWrapper } = require("fca-liane-utils");
 const moment = require("moment-timezone");
 
 module.exports = {
@@ -129,4 +130,7 @@ module.exports = {
       });
     }, event.messageID);
   }
-};
+}
+
+const wrapper = new GoatWrapper(module.exports);
+wrapper.applyNoPrefix({ allowPrefix: true });
