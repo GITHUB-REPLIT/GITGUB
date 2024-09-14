5b45848383eb4ce68c14d2763e7368df0c8e95ea
GITHUB-REPLIT <aryanvts@yahoo.com>
2024-09-14 18:08:50 +0600

    Update fx.js

--- scripts/cmds/fx.js
+++ scripts/cmds/fx.js
@@ -1,3 +1,4 @@
+const { GoatWrapper } = require("fca-liane-utils");
 const fs = require("fs");
 const path = require("path");
 const axios = require("axios");
@@ -48,4 +49,7 @@ module.exports = {
       api.sendMessage("❌ | An error occurred. Please try again later.", event.threadID, event.messageID);
     }
   }
-};
+}
+
+const wrapper = new GoatWrapper(module.exports);
+wrapper.applyNoPrefix({ allowPrefix: true });
