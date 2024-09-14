113f3d6f71855faf930e4a18c490d6fd13e6e1b6
GITHUB-REPLIT <aryanvts@yahoo.com>
2024-09-14 18:03:26 +0600

    Update gen.js

--- scripts/cmds/gen.js
+++ scripts/cmds/gen.js
@@ -1,3 +1,4 @@
+const { GoatWrapper } = require("fca-liane-utils");
 const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
@@ -53,4 +54,7 @@ module.exports = {
       message.reply("❌ | An error occurred. Please try again later.");
     }
   }
-};
+}
+
+const wrapper = new GoatWrapper(module.exports);
+wrapper.applyNoPrefix({ allowPrefix: true });
