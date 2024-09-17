d5cf914541d3d7e179c09195e93e0daf1fb75fc5
GITHUB-REPLIT <aryanvts@yahoo.com>
2024-09-17 17:07:31 +0600

    Update aryan.js

--- scripts/cmds/aryan.js
+++ scripts/cmds/aryan.js
@@ -1,9 +1,8 @@
-const { GoatWrapper } = require("fca-liane-utils");
 module.exports = {
     config: {
         name: "aryan",
         version: "1.0",
-        author: "ArYa", //** original author fb I'd : https://m.me/MR.AYAN.2X **//
+        author: "ArYan",
         countDown: 5,
         role: 0,
         shortDescription: "No Prefix",
@@ -16,9 +15,12 @@ onChat: async function({
     message,
     getLang
 }) {
-    if (event.body && event.body.toLowerCase() == "Aryan") return message.reply("ARYAN ALMIGHTY LORD YHWACH IS BUSY PLEASE WAIT 👑");
-}
-}
+    if (event.body && event.body.toLowerCase() == "aryan") return message.reply("╭─────❃\n│🤷‍♂️ 𝙰𝙿𝙽𝙰𝚁 𝙰𝙼𝙰𝚁 𝙱𝙾𝚂𝚂\n│💔 𝙰𝚁𝚈𝙰𝙽 𝙺𝙴 𝙿𝚁𝙾𝚈𝙾𝙹𝙾𝙽\n╰────────────✦");
     
-const wrapper = new GoatWrapper(module.exports);
-wrapper.applyNoPrefix({ allowPrefix: true });
+     if (event.body && event.body.toLowerCase() == "@Ar Yan") return message.reply("╭─────❃\n│𝙰𝚂𝚂𝙰𝙻𝙰𝙼𝚄 𝙰𝙻𝙰𝙸𝙺𝚄𝙼\n│🤷‍♂️ 𝙰𝙺𝙷𝙾𝙽 𝙰𝙼𝙰𝚁 𝙱𝙾𝚂𝚂\n│💔 𝙰𝚁𝚈𝙰𝙽 𝙾𝙽𝙴𝙺 𝙱𝚄𝚂𝚈 \n│𝙺𝙸 𝙱𝙾𝙻𝙱𝙴𝙽 𝙰𝙼𝙰𝙺𝙴\n│𝙱𝙾𝙻𝚃𝙴 𝙿𝙰𝚁𝙴𝙽\n╰────────────✦");
+     
+     if (event.body && event.body.toLowerCase() == "ariyan") return message.reply("╭─────❃\n│🤷‍♂️ 𝙰𝙼𝙰𝚁 𝙱𝙾𝚂𝚂 𝙺𝙴 𝙳𝙰𝙺𝙾\n│𝙺𝙴𝙽 𝙿𝚁𝙴𝙼 𝙺𝙾𝚁𝙱𝙰 𝙽𝙰𝙺𝙸\n│🫢🫣\n╰────────────✦");
+     
+     if (event.body && event.body.toLowerCase() == "ArYan") return message.reply("╭─────❃\n│🤷‍♂️ 𝙰𝙼𝙰𝚁 𝙱𝙾𝚂𝚂 𝙺𝙴 𝙳𝙰𝙺𝙾\n│𝙺𝙴𝙽 𝙿𝚁𝙴𝙼 𝙺𝙾𝚁𝙱𝙰 𝙽𝙰𝙺𝙸│🫢🫣\n╰────────────✦");
+    }
+    }
